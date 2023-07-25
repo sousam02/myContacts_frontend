100ccfde260ab59322e79e0ae1937b8c3663778f
sousam02 <moises.sousa583@gmail.com>
2023-07-25 20:36:20 -0300

    Configuring editorconfig and eslint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -7,6 +7,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
-
+  document.getElementById('root'),
+);
