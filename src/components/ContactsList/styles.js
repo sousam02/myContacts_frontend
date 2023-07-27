e48023598f6d75a4ab2db67d53ae7357602230a8
sousam02 <moises.sousa583@gmail.com>
2023-07-27 18:42:13 -0300

    Creating and styling contact card

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -10,7 +10,6 @@ export const Header = styled.header`
     justify-content: space-between;
 
     strong {
-        color: #222;
         font-size: 24px;
     }
 
@@ -26,6 +25,73 @@ export const Header = styled.header`
         &:hover {
             background: ${({ theme }) => theme.colors.primary.main};
             color: ${({ theme }) => theme.colors.background}
+        }
+    }
+`;
+
+export const ListContainer = styled.div`
+    margin-top: 24px;
+
+    header {
+        margin-bottom: 16px;
+        button {
+            background: transparent;
+            border: none;
+            display: flex;
+            align-items: center;
+        }
+
+        span {
+            font-weight: bold;
+            margin-right: 8px;
+            color: ${({ theme }) => theme.colors.primary.main};
+        }
+    }
+`;
+
+export const Card = styled.div`
+    background-color: #fff;
+    padding: 16px;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    & + & {
+        margin-top: 16px;
+    }
+
+    .info{
+        .contact-name{
+            display: flex;
+            align-items: center;
+
+            small {
+                background: ${({ theme }) => theme.colors.primary.lighter};
+                color: ${({ theme }) => theme.colors.primary.main};
+                font-weight: bold;
+                text-transform: uppercase;
+                margin-left: 8px;
+                padding: 4px;
+                border-radius: 4px;
+            }
+        }
+
+        span {
+            display: block;
+            font-size: 14px;
+            color: ${({ theme }) => theme.colors.gray[200]};
+        }
+    }
+
+    .actions {
+        display: flex;
+        align-items: center;
+
+        button {
+            background: transparent;
+            border: none;
+            margin-left: 8px;
         }
     }
 `;
