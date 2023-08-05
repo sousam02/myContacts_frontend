d17f8fb4f8ed273620fc5af0e13a81d60f8d7638
sousam02 <moises.sousa583@gmail.com>
2023-08-04 22:06:50 -0300

    Creating React Portals to display modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,26 +1,31 @@
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 import Button from '../Button';
 import { Overlay, Container, Footer } from './styles';
 
 export default function Modal({ danger }) {
   return (
-    <Overlay>
-      <Container danger={danger}>
-        <h1>
-          TItulo do modal
-        </h1>
+    ReactDOM.createPortal(
+      <Overlay>
+        <Container danger={danger}>
+          <h1>
+            TItulo do modal
+          </h1>
 
-        <p>
-          Descrição
-        </p>
+          <p>
+            Descrição
+          </p>
 
-        <Footer>
-          <button className="cancel-button" type="button">Cancelar</button>
-          <Button danger={danger}>Deletar</Button>
-        </Footer>
+          <Footer>
+            <button className="cancel-button" type="button">Cancelar</button>
+            <Button danger={danger}>Deletar</Button>
+          </Footer>
 
-      </Container>
-    </Overlay>
+        </Container>
+      </Overlay>,
+      document.getElementById('modal-root'),
+    )
+
   );
 }
 
